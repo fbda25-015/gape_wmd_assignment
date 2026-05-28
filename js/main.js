629686d5febc1d5fdf17720e9d2bae533dd1e7d7
fbda25-015 <fbda25-015@thuto.bac.ac.bw>
2026-05-28 10:56:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.getElementById('theme-toggle');
-
-  if (toggleBtn) {
-    const currentTheme = localStorage.getItem('theme');
-
-    if (currentTheme) {
-      document.documentElement.setAttribute('data-theme', currentTheme);
-      if (currentTheme === 'dark') {
-        toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
-      }
-    }
-
-    toggleBtn.addEventListener('click', () => {
-      let theme = document.documentElement.getAttribute('data-theme');
-      let newTheme = theme === 'dark' ? 'light' : 'dark';
-
-      document.documentElement.setAttribute('data-theme', newTheme);
-      localStorage.setItem('theme', newTheme);
-
-      if (newTheme === 'dark') {
-        toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
-      } else {
-        toggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
-      }
-    });
-  }
-
   const observerOptions = {
     root: null,
     rootMargin: '0px',
